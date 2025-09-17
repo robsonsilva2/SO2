ddac100f94455a4bb4b308a8869669305707aad4
robsonsilva2 <167126724+robsonsilva2@users.noreply.github.com>
2025-09-17 16:48:04 -0300

    Update LAB04.sh

--- LAB04.sh
+++ LAB04.sh
@@ -1,1 +1,22 @@
+#Etapa 1: Instalar e Configurar o Servidor Web (Apache)
+
+sudo apt-get update
+sudo apt-get install apache2
+
+echo "<h1>Bem-vindo ao Servidor Web da Empresa!</h1>" | sudo tee /var/www/html/index.html
+
+http: 10.0.2.15
+
+
+#Etapa 2: Instalar e Configurar o Servidor DNS (BIND9)
+
+sudo apt-get install bind9
+
+sudo nano /etc/bind/named.conf.local
+
+zone "empresa.loca"{
+type master;
+file "/etc/bind/db.empresa.local";
+};
+
 
