c21d2b02e3110b0d5b64c937bd2790ddb34419fd
robsonsilva2 <167126724+robsonsilva2@users.noreply.github.com>
2025-09-19 16:48:30 -0300

    Update LAB05.sh

--- LAB05.sh
+++ LAB05.sh
@@ -1,1 +1,40 @@
+#Etapa 1: Configurar o Firewall com IPTABLES
+
+nano firewall.sh
+# !/bin/bash
+echo "Aplicando regras de firerall..."
+
+# Limpa todas as regras antigas
+iptables -F
+iptables -X
+
+# Define a politica padrão: bloquear tudo que entra
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# Permite conexões já estabelecidas
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Permite acesso local
+iptables -A INPUT -i lo -j ACCEPT
+
+# SSH (porta 2244)
+iptables -A INPUT -p tcp --dport 2244 -j ACCEPT
+
+# Servidor Web (porta 80)
+iptables -A INPUT -p tcp --dport 80  - j ACCEPT
+
+# Servidor DNS (porta 53)
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+
+echo "Firewall configurado!"
+iptables -L -n -v
+
+chmod +x firewall.sh
+sudo
+
+
+
 
