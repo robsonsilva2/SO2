7df8a64ab1093dda6d05328e4087ca6b899386c8
robsonsilva2 <167126724+robsonsilva2@users.noreply.github.com>
2025-09-19 10:12:16 -0300

    Update LAB04.sh

--- LAB04.sh
+++ LAB04.sh
@@ -1,22 +1,61 @@
 #Etapa 1: Instalar e Configurar o Servidor Web (Apache)
 
+#Atualiza os pacotes e instala o Apache2:
 sudo apt-get update
 sudo apt-get install apache2
 
+#Criando uma página de teste simples:
 echo "<h1>Bem-vindo ao Servidor Web da Empresa!</h1>" | sudo tee /var/www/html/index.html
 
+#Teste de acesso via navegador:
 http: 10.0.2.15
 
 
 #Etapa 2: Instalar e Configurar o Servidor DNS (BIND9)
 
+#Instalando o BIND9:
 sudo apt-get install bind9
 
+#Configurando uma nova zona no BIND: 
 sudo nano /etc/bind/named.conf.local
 
 zone "empresa.loca"{
 type master;
 file "/etc/bind/db.empresa.local";
 };
+
+#Criando o arquivo da zona de pesquisa direta:
+sudo nano /etc/bind/db.empresa.local
+
+$TTL604800
+@IN SOA ns1.empresa.local. root.empresa.local. (
+2 ; Serial
+604800 ; Refresh
+86400 ; Retry
+2419200 ; Expire
+604800 ) ; Negative Cache TTL
+:
+@IN NS ns1.empresa.local.
+ns1 IN A 10.0.2.15
+www IN A 10.0.2.15
+
+#Etapa 3: Reiniciar, Configurar Cliente e Testar
+
+#Reiniciando o serviço BIND:
+sudo  systemctl restart bind9
+
+#Configurando o DNS da VM em:
+nano /etc/resolv.conf:
+
+#Adicionado no topo:
+nameserver 127.0.0.53
+
+#Testando a soloção de nome:
+dig www.empresa.local
+
+#Teste final com curl:
+curl http://www.empresa.local
+
+
 
 
